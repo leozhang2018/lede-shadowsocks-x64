d9e9e7ed598674d546c38adac72b47ad9f80e7d8
leozhang2018 <leozhang2018@gmail.com>
2020-08-05 12:54:39 +0000

    update pkg

--- shadowsocks-x64/config.json.js
+++ shadowsocks-x64/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"2.2.3",
-"md5":"d7f02ccb85f360c08456b77de199f237",
+"version":"2.2.4",
+"md5":"9990b7c78e9fbdceb7af01c5b4c3787c",
 "home_url":"Module_koolss.asp",
 "title":"科学上网插件",
 "description":"轻松科学上网~",
-"changelog":"更新二进制，同步最新规则",
+"changelog":"修复 rules 地址",
 "build_date":"2020-08-05_20:17:10"
 }
